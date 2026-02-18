008e268c4123af122b6ddf645fed6807b57d2944
Lugia05 <jossue1437@gmail.com>
2026-02-18 17:17:15 +0000

    2/18/2026 Attendance

--- app/src/main/java/org/csu/cpsc/basic_data_structures/MyLinkedList.java
+++ app/src/main/java/org/csu/cpsc/basic_data_structures/MyLinkedList.java
@@ -230,182 +230,99 @@ public class MyLinkedList<E> implements MyListInterface<E> {
         return ArList.toString() ;
     }
 
+    //public boolean remove(Object o){
 
-}
+    //}
+    /*
+    @Override
+    public E set(int index, E element){
+    //valid index
+        if(index < 0 || index > size()){
+            throw new IndexOutOfBoundsException("Unable to set up value");
+        } else {
+            Node currentNode = head;
+            int position = 0;
 
-
-/*
-package org.csu.cpsc.basic_data_structures
-
-public class MyLinkedList<E> implements MylistInterface<E>{
-    private class MyLinkedlist<E> implements MyListInterface<E>{
-        private Node<E> head;
-
-        public myLinkedList(){
-            this.head = null
-        }
-
-        @Override
-        public E set(int index, E element){
-            if(index >= 0 && index < size()){
-                int position = 0;
-                Node<E> currentNode = head;
-
-                while(position < index){
-                    currentNode = currentNode.next;
-                    position ++;
-                }
-                currentNode.data = E element;
-                return currentNode.data;
-            } else {
-                throw IndexOutOfBoundsException(s: "Unable to get");
+            while(position < index){
+                position++;
+                currentNode= currentNode.next;
             }
 
+            // 1. Save the data to a temporary object to return
+            E tempData = currentNode.data;
+
+            //2. Set the data in the object
+            currentNode.data = element;
+
+            //3. Return the old data
+            return tempData;
         }
 
         @Override
         public void add(int index, E element){
-            if(index >= 0 && index <= size()){
-                int position = 0;
-                Node<E> currentNode = head;
-                //checks if the index is not at 0 before starting
-                if (index = 0){
-                //checks if the index is 0
-                    
-                } else if (index = size()){
-                    //checks if the index is at the end
-                    while(position != index){
-                        currentNode = currentNode.next;
-                        position ++;
-                    }
+            //Bevahior: insert a new node (element) at the index
+            //Parameters:
+            // int index: The index where the object should be inserted
+            // E element: The data for the new object
+
+            // Is it a valid index
+            if(index < 0 || index >= size()){
+                throw new IndexOutOfBoundsException();
+            } else {
+                Node<E> newNode = new Node<E>(element);
+                if(index == 0){
+                    newNode.next = head;
+                    head = newNode;
+                } else if (index == size()){
                     add(element);
+                } else {
+                    Node<E> currentNode = head;
+                    int position = 0;
 
-                } else{
-                    //default to this if the index is not 0 nor at the end
-                    while(position < index){
-                        currentNode = currentNode.next;
-                        position ++;
-                    }
-                }
-                //add here
-
-            } else {
-                throw IndexOutOfBoundsException(s: "Unable to get");
-            }
-        }
-
-
-        //below is stuff before the assignment
-
-        @Override
-        public boolean add(E element){
-            Node<E> newNode = new Node<E>(element);
-
-            if(head.next == null){
-                this.head = newNode;
-            } else {
-                Node<E> currentNode = head;
-                while(currentNode.head.next = null){
-                    currentNode = currentNode.next;
-                }
-                currentNode.next = newNode;
-            }
-
-            return true;
-        }
-
-        public boolean contains(Object o){
-            if(head != null){
-                Node<E> currentNode = new Node<E>();
-
-                while(currentNode.next != null){
-                    if (currentNode.data.equals(o)){
-                        return true;
-                    } else {
-                        currentnode = currentNode.next ;
-                    }
-                }
-            }
-            //check placement
-            throw IndexOutOfBoundsException(s: "Unable to get");
-        }
-
-        @Override
-        public int size(){
-            Node<E> currentNode = head;
-            int size = 0;
-            while (currentNode != null){
-                size += 1;
-                currentNode = currentNode.next;
-            }
-            return size;
-        }
-
-        @Override
-        public int indexOf(Object o){
-            int index = -1;
-
-            if(head 1= null){
-                index = 0;
-
-                Node<E> currentNode = head;
-                while(currentNode != null){
-                    if(currentNode.equals(o)){
-                        return index;
-                    } else {
-                        index++;
+                    while(position < index - 1){
+                        position++;
                         currentNode = currentNode.next;
                     }
 
+                    newNode.next = currentNode.next;
+                    currentNode.next = newNode;
                 }
-                return index;
             }
         }
+    }
 
-        @Override
-        public E get(int index){
-            if(index >= 0 && index < size()){
+    public E remove(int index){
+        if (index < 0 || index >= size()){
+            throw new IndexOutOfBoundsException();
+        } else {
+            E returnValue = null;
+
+            if(index == 0){
+                Node<E> temp = head;
+                head = head.next;
+                temp.next = null;
+                returnValue = temp.data;
+            } else {
+                Node<E> currentNode = head;
                 int position = 0;
-                Node<E> currentNode = head;
 
-                while(position < index){
+                while (position < index -1 ){
+                    position++;
                     currentNode = currentNode.next;
-                    position ++;
                 }
-                return currentNode.data;
-            } else {
-                throw IndexOutOfBoundsException(s: "Unable to get");
+
+                Node<E> temp = currentNode.next;
+                currentNode.next = currentNode.nexrt.next;
+
+                temp.next = null;
+                returnValue = temp.data;
             }
 
+            return returnValue;
         }
-
-        private class Node<E>{
-            E data;
-            Node<E> next;
-
-            Node(E data){
-                this.data = data;
-                next = null;
-            }
-        }
-
-
     }
+    
+    */
+
+
 }
-
-/* public void traverse(){
-    //start at head
-    Node current = head;
-
-    //Loop until end
-    while (current != null){
-        System.out.print(*current.data=())
-    }
-}
-
-Step 1 current = head
-Step 2
-
-last note points to null
-transversal uses a loop.
- */
